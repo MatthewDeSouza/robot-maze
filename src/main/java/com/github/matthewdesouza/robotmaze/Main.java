4696982303079adaa7375f44382e2bbb026683d4
SuperNovaXYZ <alimn2@farmingdale.edu>
2023-09-30 01:18:26 -0400

    New Branch Changes

--- src/main/java/com/github/matthewdesouza/robotmaze/Main.java
+++ src/main/java/com/github/matthewdesouza/robotmaze/Main.java
@@ -1,28 +1,34 @@
 package com.github.matthewdesouza.robotmaze;
 
-//import com.sun.javafx.stage.EmbeddedWindow;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.util.Duration;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tooltip;
+import javafx.scene.input.KeyCode;
+
+
+
 
 /**
  * The Main class is the entry point for the JavaFX application.
- * It creates a window containing a TabPane with a two tabs that displays a robot maze, and car maze.
+ * It creates a window containing a TabPane with a single tab that displays a Maze.
  * <p/>
+ * TODO: Implement Car racing {@link Tab}.
  *
  * @author Matthew DeSouza
  */
 public class Main extends Application {
-    // The Maze instance representing the robot maze and car maze.
+    // The Maze instance representing the robot maze.
     private Maze robotMaze;
-    private Maze2 carMaze;
 
     /**
      * The main method to launch the application.
@@ -43,35 +49,23 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         // Initialize the Maze with the specified images.
         robotMaze = new Maze("maze.png", "robot.png");
-        carMaze = new Maze2("maze2.png","car.png");
 
-        // Create a TabPane and add a Tab with the robot Maze.
+        // Create a TabPane and add a Tab with the Maze.
+        // TODO: Create carMaze Tab, add it to tabPane.
         TabPane tabPane = new TabPane();
         Tab robotMazeTab = new Tab("Robot Maze", robotMaze);
         robotMazeTab.setClosable(false); // The tab cannot be closed by the user.
         tabPane.getTabs().add(robotMazeTab);
 
-        // Create car Maze Tab, add it to tabPane.
-        Tab carMazeTab = new Tab("Car Maze", carMaze);
-        carMazeTab.setClosable(false); // The tab cannot be closed by the user.
-        tabPane.getTabs().add(carMazeTab);
-
         // Create a VBox to vertically stack the TabPane and the Maze.
+        // TODO: Modify VBox to include carMaze.
         VBox root = new VBox(tabPane, robotMaze);
-        VBox root2 = new VBox(tabPane, carMaze);
 
         // Create a Scene with the VBox as the root node.
         Scene scene = new Scene(root, robotMaze.getWidth(), robotMaze.getHeight() + tabPane.getTabMaxHeight());
+
         // Add an event filter to handle key presses.
         scene.addEventFilter(KeyEvent.KEY_PRESSED, this::onKeyPressed);
-
-        //Draw car
-        Rectangle rect = new Rectangle();
-        Circle frontWheel = new Circle();
-        Circle rearWheel = new Circle();
-        rect.setFill(Color.LIGHTPINK);
-        frontWheel.setFill(Color.BLACK);
-        rearWheel.setFill(Color.BLACK);
 
         // Set up the primary stage.
         primaryStage.setScene(scene);
@@ -80,18 +74,24 @@ public class Main extends Application {
         primaryStage.sizeToScene();
         primaryStage.show();
 
-        // Create a Scene with the VBox as the root node.
-        Scene scene2 = new Scene(root2, carMaze.getWidth(), carMaze.getHeight() + tabPane.getTabMaxHeight());
-        // Add an event filter to handle key presses.
-        scene2.addEventFilter(KeyEvent.KEY_PRESSED, this::onKeyPressd);
+        // Add the Alt+P key combination listener here
+        scene.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
+            if (event.isAltDown() && event.getCode() == KeyCode.P) {
+                animateRobot(); // Start the animation when Alt+P is pressed
+            }
+        });
+    }
 
-        // set up car maze stage
-        Stage stage = new Stage();
-        stage.setScene(scene2);
-        stage.setTitle("CAR Maze");
-        stage.setResizable(true);
-        stage.sizeToScene();
-        stage.show();
+    /**
+     * create button
+     */
+
+    private Button createStartAnimationButton() {
+        Button startAnimationButton = new Button("Start Animation (P)");
+        startAnimationButton.setMnemonicParsing(true); // Enable mnemonic parsing
+        startAnimationButton.setTooltip(new Tooltip("Press Alt+P to start animation"));
+        startAnimationButton.setOnAction(event -> animateRobot());
+        return startAnimationButton;
     }
 
     /**
@@ -122,27 +122,24 @@ public class Main extends Application {
                 break;
         }
     }
-    private void onKeyPressd(KeyEvent event) {
-        switch (event.getCode()) {
-            case UP:
-            case W:
-                carMaze.moveCar(0, -1); // Move up
-                break;
-            case DOWN:
-            case S:
-                carMaze.moveCar(0, 1); // Move down
-                break;
-            case LEFT:
-            case A:
-                carMaze.moveCar(-1, 0); // Move left
-                break;
-            case RIGHT:
-            case D:
-                carMaze.moveCar(1, 0); // Move right
-                break;
-            case R:
-                carMaze.resetCar(); // Reset the car position
-                break;
-        }
+
+    /**
+     * Nova Alim animate robot method
+     */
+    private void animateRobot() {
+        Timeline timeline = new Timeline();
+        timeline.setCycleCount(Animation.INDEFINITE); // or a specific number of cycles if needed
+
+        KeyFrame keyFrame = new KeyFrame(Duration.millis(100), event -> {
+            // Calculate the next move for the robot and move it.
+            // Stop the animation if the robot reaches the end or cannot move further.
+            if (!robotMaze.moveRobotToNextPosition()) {
+                timeline.stop();
+            }
+        });
+
+        timeline.getKeyFrames().add(keyFrame);
+        timeline.play();
     }
+
 }
--- src/main/java/com/github/matthewdesouza/robotmaze/Maze.java
+++ src/main/java/com/github/matthewdesouza/robotmaze/Maze.java
@@ -1,55 +1,34 @@
 package com.github.matthewdesouza.robotmaze;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.image.PixelReader;
 import javafx.scene.layout.Pane;
+import javafx.util.Duration;
 import javafx.scene.paint.Color;
+import javafx.scene.image.PixelReader;
 
 import java.util.Objects;
 
-/**
- * The Maze class is a graphical representation of a maze using JavaFX.
- * It handles the movement and positioning of a robot within the maze.
- *
- * @author Matthew DeSouza
- */
 public class Maze extends Pane {
-
-    // Initial positions of the robot within the maze.
     private static final int INITIAL_X_POS = 16;
     private static final int INITIAL_Y_POS = 260;
-
-    // Robot image's dimensions
     private static final int ROBOT_X = 25;
     private static final int ROBOT_Y = 25;
-
-    // Pixel step value
     private static final int PIXEL_STEP = 5;
 
-    // Image representing the maze.
     private final Image mazeImage;
-
-    // ImageView representing the robot.
     private final ImageView robotView;
-
-    // Current positions of the robot within the maze.
     private int robotX;
     private int robotY;
 
-    /**
-     * Constructs a Maze object and initializes its graphical components.
-     *
-     * @param mazePath  the path to the maze image resource.
-     * @param robotPath the path to the robot image resource.
-     */
     public Maze(String mazePath, String robotPath) {
-        // Load and display the maze image.
         mazeImage = new Image(Objects.requireNonNull(getClass().getResourceAsStream(mazePath)));
         ImageView mazeView = new ImageView(mazeImage);
         getChildren().add(mazeView);
 
-        // Load and display the robot image at its initial position.
         Image robotImage = new Image(Objects.requireNonNull(getClass().getResourceAsStream(robotPath)));
         robotView = new ImageView(robotImage);
         robotX = INITIAL_X_POS;
@@ -59,21 +38,11 @@ public class Maze extends Pane {
         getChildren().add(robotView);
     }
 
-    /**
-     * Moves the robot within the maze.
-     *
-     * @param dx the amount to move in the x direction.
-     * @param dy the amount to move in the y direction.
-     */
     public void moveRobot(int dx, int dy) {
-        // Calculate the step size for the movement.
         int stepX = dx != 0 ? (int) Math.signum(dx) * PIXEL_STEP : 0;
         int stepY = dy != 0 ? (int) Math.signum(dy) * PIXEL_STEP : 0;
-
-        // Calculate the number of steps to move.
         int steps = Math.max(1, Math.max(Math.abs(dx) / PIXEL_STEP, Math.abs(dy) / PIXEL_STEP));
 
-        // Move the robot step by step, stopping if an invalid move is detected.
         for (int i = 0; i < steps; i++) {
             int newX = robotX + stepX;
             int newY = robotY + stepY;
@@ -90,9 +59,6 @@ public class Maze extends Pane {
         }
     }
 
-    /**
-     * Resets the robot to its initial position within the maze.
-     */
     public void resetRobot() {
         robotX = INITIAL_X_POS;
         robotY = INITIAL_Y_POS;
@@ -100,23 +66,31 @@ public class Maze extends Pane {
         robotView.setY(INITIAL_Y_POS);
     }
 
-    /**
-     * Determines whether the proposed move is valid.
-     *
-     * @param x the x-coordinate of the proposed new position.
-     * @param y the y-coordinate of the proposed new position.
-     * @return true if the move is valid, false otherwise.
-     */
+    public boolean moveRobotToNextPosition() {
+        // Calculate the next position based on your logic.
+        int nextX = robotX + PIXEL_STEP;
+        int nextY = robotY;
+
+        // Check if the move is valid.
+        if (isValidMove(nextX, nextY)) {
+            robotX = nextX;
+            robotY = nextY;
+            robotView.setX(robotX);
+            robotView.setY(robotY);
+            return true; // The robot can move.
+        } else {
+            return false; // The robot cannot move further.
+        }
+    }
+
     private boolean isValidMove(int x, int y) {
         PixelReader pixelReader = mazeImage.getPixelReader();
 
-        // Check each pixel in the robot picture's area that the robot will occupy after the move.
         for (int i = 0; i < ROBOT_X; i++) {
             for (int j = 0; j < ROBOT_Y; j++) {
                 int pixelX = x + i;
                 int pixelY = y + j;
 
-                // If the pixel is outside the maze or is not white, the move is invalid.
                 if (pixelX < 0 || pixelX >= mazeImage.getWidth() || pixelY < 0 || pixelY >= mazeImage.getHeight() ||
                         !pixelReader.getColor(pixelX, pixelY).equals(Color.WHITE)) {
                     return false;
@@ -124,7 +98,23 @@ public class Maze extends Pane {
             }
         }
 
-        // If all the pixels in the 25x25 area are white, the move is valid.
         return true;
+    }
+
+    public void startRobotAnimation() {
+        Timeline timeline = new Timeline(); // Initialize the timeline
+
+        timeline.getKeyFrames().add(
+                new KeyFrame(Duration.millis(100), event -> {
+                    // Calculate the next move for the robot and move it.
+                    // Stop the animation if the robot reaches the end or cannot move further.
+                    if (!moveRobotToNextPosition()) {
+                        timeline.stop();
+                    }
+                })
+        );
+
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
     }
 }
